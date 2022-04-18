62e5205fb2cec00924af06634add034b61228811
Marina Zinchenko <iolsoofuolso@gmail.com>
2022-04-18 00:14:34 -0700

    add tests for getHiddenFilesCount func

---
+++ __tests__/getHiddenFilesCount.test.js
@@ -0,0 +1,55 @@
+import {
+  mkdir, mkfile,
+} from '@hexlet/immutable-fs-trees';
+import getHiddenFilesCount from '../src/getHiddenFilesCount.js';
+
+test('given a tree with hidden files and hidden dirs', () => {
+  const tree = mkdir('/', [
+    mkdir('city-of-tears', [
+      mkdir('.city-storeroom'),
+      mkdir('king-station', [
+        mkfile('.soul-sanctum.conf', { size: 800 }),
+      ]),
+      mkdir('.fungal-wastes', [
+        mkfile('.mantis-village.json', { size: 1200 }),
+        mkfile('fungal-core', { size: 8200 }),
+        mkfile('queen-station', { size: 80 }),
+      ]),
+    ]),
+    mkfile('.greenpath', { size: 3500 }),
+    mkfile('ancient-basin', { size: 1000 }),
+  ]);
+  const count = getHiddenFilesCount(tree);
+  expect(count).toEqual(3);
+});
+
+test('given a tree with no hidden files', () => {
+  const tree = mkdir('/', [
+    mkdir('city-of-tears', [
+      mkdir('.city-storeroom'),
+      mkdir('king-station', [
+        mkfile('soul-sanctum.conf', { size: 800 }),
+      ]),
+      mkdir('.fungal-wastes', [
+        mkfile('mantis-village.json', { size: 1200 }),
+        mkfile('fungal-core', { size: 8200 }),
+        mkfile('queen-station', { size: 80 }),
+      ]),
+    ]),
+    mkfile('greenpath', { size: 3500 }),
+    mkfile('ancient-basin', { size: 1000 }),
+  ]);
+  const count = getHiddenFilesCount(tree);
+  expect(count).toEqual(0);
+});
+
+test('given an emptyTree', () => {
+  const emptyTree = {
+    name: undefined,
+    children: [],
+    meta: {},
+    type: 'directory',
+  };
+  const count = getHiddenFilesCount(emptyTree);
+  expect(count).toEqual(0);
+});

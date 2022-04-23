4ae2135808d24e80009e6145234251b65c36a9d8
Marina Zinchenko <iolsoofuolso@gmail.com>
2022-04-23 00:06:04 -0700

    add findEmptyDirNames funcs and tests for them

---
+++ __tests__/findEmptyDirNames.test.js
@@ -0,0 +1,57 @@
+import {
+  mkdir, mkfile,
+} from '@hexlet/immutable-fs-trees';
+import {
+  findEmptyDirNamesReduce,
+  findEmptyDirNamesMap,
+  findEmptyDirNamesComplex1,
+  findEmptyDirNamesComplex2,
+} from '../src/findEmptyDirNames.js';
+
+const tree = mkdir('/', [
+  mkdir('etc', [
+    mkdir('apache'),
+    mkdir('nginx', [
+      mkfile('nginx.conf'),
+    ]),
+    mkdir('consul', [
+      mkfile('config.json'),
+      mkdir('data'),
+    ]),
+  ]),
+  mkdir('logs'),
+  mkfile('hosts'),
+]);
+
+const emptyDir = mkdir('empty', []);
+
+const file = { name: 'file1', meta: { size: 125 }, type: 'file' };
+
+describe('testing simple version of findEmptyDirNames', () => {
+  test('receives non-empty tree', () => {
+    expect(findEmptyDirNamesMap(tree)).toEqual(['apache', 'data', 'logs']);
+    expect(findEmptyDirNamesReduce(tree)).toEqual(['apache', 'data', 'logs']);
+  });
+
+  test('receives empty tree', () => {
+    expect(findEmptyDirNamesMap(emptyDir)).toEqual('empty');
+    expect(findEmptyDirNamesReduce(emptyDir)).toEqual('empty');
+  });
+
+  test('receives file', () => {
+    expect(findEmptyDirNamesMap(file)).toEqual([]);
+    expect(findEmptyDirNamesReduce(file)).toEqual([]);
+  });
+});
+
+describe('testing complex version of findEmptyDirNames', () => {
+  test('receives non-empty tree and depth of 2', () => {
+    expect(findEmptyDirNamesComplex1(tree, 2)).toEqual(['apache', 'logs']);
+    expect(findEmptyDirNamesComplex2(tree, 2)).toEqual(['apache', 'logs']);
+  });
+
+  test('receives non-empty tree and default depth', () => {
+    expect(findEmptyDirNamesComplex1(tree)).toEqual(['apache', 'data', 'logs']);
+    expect(findEmptyDirNamesComplex2(tree)).toEqual(['apache', 'data', 'logs']);
+  });
+});
---
+++ src/findEmptyDirNames.js
@@ -0,0 +1,108 @@
+import {
+  getChildren, getName, isFile, isDirectory,
+} from '@hexlet/immutable-fs-trees';
+
+export const findEmptyDirNamesReduce = (tree) => {
+  if (isFile(tree)) {
+    return [];
+  }
+  const children = getChildren(tree);
+  if (children.length === 0) {
+    return getName(tree);
+  }
+  const emptyDirsCount = children.reduce((acc, el) => {
+    acc.push(findEmptyDirNamesReduce(el));
+    return acc;
+  }, []);
+  return emptyDirsCount.flat();
+};
+
+export const findEmptyDirNamesMap = (tree) => {
+  if (isFile(tree)) {
+    return [];
+  }
+  const children = getChildren(tree);
+  if (children.length === 0) {
+    return getName(tree);
+  }
+  const emptyDirsCount = children.map(findEmptyDirNamesMap);
+  return emptyDirsCount.flat();
+};
+
+// SAMPLE SOLUTION:
+/*
+const findEmptyDirNames = (tree) => {
+  const name = getName(tree);
+  const children = getChildren(tree);
+  if (children.length === 0) {
+    return name;
+  }
+
+  const emptyDirNames = children.filter((child) => !isFile(child))
+    .flatMap(findEmptyDirNames);
+
+  return emptyDirNames;
+};
+
+findEmptyDirNames(tree); // ['apache', 'data', 'logs']
+*/
+
+export const findEmptyDirNamesComplex1 = (tree, maxDepth = Infinity) => {
+  const iter = (node, acc) => {
+    if (acc > maxDepth) {
+      return [];
+    }
+    if (isFile(node)) {
+      return [];
+    }
+    const children = getChildren(node);
+    if (children.length === 0) {
+      return getName(node);
+    }
+    const emptyDirNames = children.map((child) => iter(child, acc + 1));
+    return emptyDirNames.flat();
+  };
+  return iter(tree, 0);
+};
+
+export const findEmptyDirNamesComplex2 = (tree, maxDepth = Infinity) => {
+  if (isFile(tree)) {
+    return [];
+  }
+  const iter = (node, acc) => {
+    if (acc > maxDepth) {
+      return [];
+    }
+    const children = getChildren(node);
+    if (children.length === 0) {
+      return getName(node);
+    }
+    const emptyDirNames = children.filter(isDirectory)
+      .map((child) => iter(child, acc + 1));
+    return emptyDirNames.flat();
+  };
+  return iter(tree, 0);
+};
+
+// SAMPLE SOLUTION:
+/*
+const findEmptyDirPaths = (tree) => {
+  const iter = (node, depth) => {
+    const name = getName(node);
+    const children = getChildren(node);
+
+    if (children.length === 0) {
+      return name;
+    }
+
+    if (depth === 2) {
+      return [];
+    }
+
+    return children.filter(isDirectory)
+      .flatMap((child) => iter(child, depth + 1));
+
+  };
+  return iter(tree, 0);
+};
+*/
